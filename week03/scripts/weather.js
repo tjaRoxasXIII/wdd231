6f85dd2cc053ca35a87765bd3cfc553783f31b90
tjaRoxasXIII <tjaroguetech@gmail.com>
2026-01-19 13:28:42 -0500

    Uploading weather API calls - Week 03 activity

---
+++ week03/scripts/weather.js
@@ -0,0 +1,33 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=e09f9c17d5f800e31905962d2c8b330d';
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+
+apiFetch();
